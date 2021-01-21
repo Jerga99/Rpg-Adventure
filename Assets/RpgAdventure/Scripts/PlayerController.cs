a75b77268be005756a01145528978641550e7216
Filip Jerga <jerga99@gmail.com>
2021-01-21 10:36:25 +0000

    Adds rotation

--- Assets/RpgAdventure/Scripts/PlayerController.cs
+++ Assets/RpgAdventure/Scripts/PlayerController.cs
@@ -7,9 +7,11 @@ namespace RpgAdventure
     public class PlayerController : MonoBehaviour
     {
         public float speed;
+        public float rotationSpeed;
 
         private Rigidbody m_Rb;
         private Vector3 m_Movement;
+        private Quaternion m_Rotation;
 
         private void Start()    
         {
@@ -24,7 +26,18 @@ namespace RpgAdventure
             m_Movement = new Vector3(horizontalInput, 0, verticalInput);
             m_Movement.Normalize();
 
+            Vector3 desiredForward = Vector3.RotateTowards(
+                transform.forward,
+                m_Movement,
+                Time.fixedDeltaTime * rotationSpeed,
+                0
+            );
+
+            m_Rotation = Quaternion.LookRotation(desiredForward);
+
+
             m_Rb.MovePosition(m_Rb.position + m_Movement * speed * Time.fixedDeltaTime);
+            m_Rb.MoveRotation(m_Rotation);
         }
     }
 }
