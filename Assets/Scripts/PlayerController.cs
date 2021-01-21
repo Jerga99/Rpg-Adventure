77c7a384d135831477265853c16d7fdcec523a39
Filip Jerga <jerga99@gmail.com>
2021-01-21 10:15:02 +0000

    Adds rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,13 @@ namespace RpgAdventure
     {
         public float speed;
 
+        private Rigidbody m_Rb;
         private Vector3 m_Movement;
+
+        private void Start()    
+        {
+            m_Rb = GetComponent<Rigidbody>();
+        }
 
         void FixedUpdate()
         {
@@ -18,7 +24,7 @@ namespace RpgAdventure
             m_Movement = new Vector3(horizontalInput, 0, verticalInput);
             m_Movement.Normalize();
 
-            transform.Translate(m_Movement * speed * Time.fixedDeltaTime);
+            m_Rb.MovePosition(m_Rb.position + m_Movement * speed * Time.fixedDeltaTime);
         }
     }
 }
