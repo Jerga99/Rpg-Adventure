190abee1e104ea34a7ea42636f8d6ec8cae57acb
Filip Jerga <jerga99@gmail.com>
2021-01-26 12:17:59 +0000

    Character controller properties

--- Assets/RpgAdventure/Scripts/Camera/CameraController.cs
+++ Assets/RpgAdventure/Scripts/Camera/CameraController.cs
@@ -1,25 +1,28 @@
 using Cinemachine;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace RpgAdventure
 {
-    [SerializeField]
-    CinemachineFreeLook freeLookCamera;
-
-    // Update is called once per frame
-    void Update()
+    public class CameraController : MonoBehaviour
     {
+        [SerializeField]
+        CinemachineFreeLook freeLookCamera;
 
-        if (Input.GetMouseButtonDown(1))
+        // Update is called once per frame
+        void Update()
         {
-            freeLookCamera.m_XAxis.m_MaxSpeed = 400;
-            freeLookCamera.m_YAxis.m_MaxSpeed = 10;
-        }
 
-        if (Input.GetMouseButtonUp(1))
-        {
-            freeLookCamera.m_XAxis.m_MaxSpeed = 0;
-            freeLookCamera.m_YAxis.m_MaxSpeed = 0;
+            if (Input.GetMouseButtonDown(1))
+            {
+                freeLookCamera.m_XAxis.m_MaxSpeed = 400;
+                freeLookCamera.m_YAxis.m_MaxSpeed = 10;
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                freeLookCamera.m_XAxis.m_MaxSpeed = 0;
+                freeLookCamera.m_YAxis.m_MaxSpeed = 0;
+            }
         }
     }
 }
