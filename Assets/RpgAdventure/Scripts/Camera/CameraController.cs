e610ad515377c48232fccf816c834ab10623daeb
Filip Jerga <jerga99@gmail.com>
2021-01-26 09:17:29 +0000

    Move camera while holding the mouse

---
+++ Assets/RpgAdventure/Scripts/Camera/CameraController.cs
@@ -0,0 +1,25 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    CinemachineFreeLook freeLookCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            freeLookCamera.m_XAxis.m_MaxSpeed = 400;
+            freeLookCamera.m_YAxis.m_MaxSpeed = 10;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            freeLookCamera.m_XAxis.m_MaxSpeed = 0;
+            freeLookCamera.m_YAxis.m_MaxSpeed = 0;
+        }
+    }
+}
