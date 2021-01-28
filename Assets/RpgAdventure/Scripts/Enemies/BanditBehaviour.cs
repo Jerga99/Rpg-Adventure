22964a0a350ca2df3f32c4b9a193ef291379a74e
Filip Jerga <jerga99@gmail.com>
2021-01-28 11:24:11 +0000

    Create a bandit

---
+++ Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace RpgAdventure
+{
+    public class BanditBehaviour : MonoBehaviour
+    {
+
+        private void Awake()
+        {
+            Debug.Log("Bandit is Awaken!");
+        }
+    }
+}
