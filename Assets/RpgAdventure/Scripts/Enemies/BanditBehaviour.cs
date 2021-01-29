7ccbd10800188d3521f6f2c89a81cbb175ed6c6c
Filip Jerga <jerga99@gmail.com>
2021-01-29 09:18:34 +0000

    Detection range

--- Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
+++ Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
@@ -4,10 +4,33 @@ namespace RpgAdventure
 {
     public class BanditBehaviour : MonoBehaviour
     {
+        public float detectionRadius = 10.0f;
 
-        private void Start()
+        private void Update()
         {
-            Debug.Log(PlayerController.Instance);
+            LookForPlayer();
+        }
+
+        private PlayerController LookForPlayer() {
+
+            if (PlayerController.Instance == null)
+            {
+                return null;
+            }
+
+            Vector3 enemyPosition = transform.position;
+            Vector3 toPlayer = PlayerController.Instance.transform.position - enemyPosition;
+            toPlayer.y = 0;
+
+            if (toPlayer.magnitude <= detectionRadius)
+            {
+                Debug.Log("Detecting the player!");
+            } else
+            {
+                Debug.Log("Where are you?");
+            }
+
+            return null;
         }
     }
 }
