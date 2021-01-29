badcdaab59f84466c46e995d530917cd6439edda
Filip Jerga <jerga99@gmail.com>
2021-01-29 10:27:15 +0000

    Use dot product to detect player

--- Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
+++ Assets/RpgAdventure/Scripts/Enemies/BanditBehaviour.cs
@@ -25,10 +25,11 @@ namespace RpgAdventure
 
             if (toPlayer.magnitude <= detectionRadius)
             {
-                Debug.Log("Detecting the player!");
-            } else
-            {
-                Debug.Log("Where are you?");
+                if (Vector3.Dot(toPlayer.normalized, transform.forward) >
+                    Mathf.Cos(detectionAngle * 0.5f * Mathf.Deg2Rad))
+                {
+                    Debug.Log("Player Has been detected!!!!");
+                }
             }
 
             return null;
@@ -41,13 +42,10 @@ namespace RpgAdventure
             Color c = new Color(0.8f, 0, 0, 0.4f);
             UnityEditor.Handles.color = c;
 
-            Debug.Log("FORWARD " + transform.forward);
             Vector3 rotatedForward = Quaternion.Euler(
                 0,
                 -detectionAngle * 0.5f,
                 0) * transform.forward;
-
-            Debug.Log("rotatedForward " + rotatedForward);
 
             UnityEditor.Handles.DrawSolidArc(
                 transform.position,
