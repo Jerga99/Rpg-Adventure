5dcbba3e3ea302f7963bd688daf10f55c0ba91a1
Filip Jerga <jerga99@gmail.com>
2021-02-04 10:23:16 +0000

    Add melee weapon script

---
+++ Assets/RpgAdventure/Scripts/Weapons/MeleeWeapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RpgAdventure
+{
+    public class MeleeWeapon : MonoBehaviour
+    {
+        public int damage = 10;
+    }
+}
