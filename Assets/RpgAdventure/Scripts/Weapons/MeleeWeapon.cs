78f6e9baab5fdf323af1fffa7d96ca2fe4873168
Filip Jerga <jerga99@gmail.com>
2021-02-05 11:00:23 +0000

    Attack points on weapon

--- Assets/RpgAdventure/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/RpgAdventure/Scripts/Weapons/MeleeWeapon.cs
@@ -6,11 +6,38 @@ namespace RpgAdventure
 {
     public class MeleeWeapon : MonoBehaviour
     {
+        [System.Serializable]
+        public class AttackPoint
+        {
+            public float radius;
+            public Vector3 offset;
+            public Transform rootTransform;
+        }
+
         public int damage = 10;
+        public AttackPoint[] attackPoints = new AttackPoint[0];
 
         public void BeginAttack()
         {
             Debug.Log("Weapon is swinging!");
         }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            foreach (AttackPoint attackPoint in attackPoints)
+            {
+                if (attackPoint.rootTransform != null)
+                {
+
+                    Vector3 worldPosition = attackPoint.rootTransform.TransformVector(attackPoint.offset);
+                    Gizmos.color = new Color(1.0f, 1.0f, 1.0f, 0.6f);
+                    Gizmos.DrawSphere(
+                        attackPoint.rootTransform.position + worldPosition,
+                        attackPoint.radius);
+                }
+            }
+        }
+#endif
     }
 }
