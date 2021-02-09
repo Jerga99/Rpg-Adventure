be7e832364d9aa099318c8fab47b73b30590cec5
Filip Jerga <jerga99@gmail.com>
2021-02-09 08:54:34 +0000

    Draw hit angle

--- Assets/RpgAdventure/Scripts/DamageSystem/Damageable.cs
+++ Assets/RpgAdventure/Scripts/DamageSystem/Damageable.cs
@@ -7,10 +7,29 @@ namespace RpgAdventure
     public partial class Damageable : MonoBehaviour
     {
         public int maxHitPoints;
+        [Range(0, 360.0f)]
+        public float hitAngle = 360.0f;
 
         public void ApplyDamage()
         {
             //Debug.Log("Applying Damage!");
         }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            UnityEditor.Handles.color = new Color(0.0f, 0.0f, 1.0f, 0.5f);
+
+            Vector3 rotatedForward =
+                Quaternion.AngleAxis(-hitAngle * 0.5f, transform.up) * transform.forward;
+
+            UnityEditor.Handles.DrawSolidArc(
+                transform.position,
+                transform.up,
+                rotatedForward,
+                hitAngle,
+                1.0f);
+        }
+#endif
     }
 }
