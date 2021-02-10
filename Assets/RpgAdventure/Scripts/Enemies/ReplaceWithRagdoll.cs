da2c0432d33d386db1a90e3bb25bd9fb85f2cb35
Filip Jerga <jerga99@gmail.com>
2021-02-10 10:10:44 +0000

    Replace with ragdoll implementation

--- Assets/RpgAdventure/Scripts/Enemies/ReplaceWithRagdoll.cs
+++ Assets/RpgAdventure/Scripts/Enemies/ReplaceWithRagdoll.cs
@@ -9,10 +9,55 @@ namespace RpgAdventure
 
         public void Replace()
         {
-            GameObject ragdolInstance = Instantiate(ragdollPrefab, transform.position, transform.rotation);
+            GameObject ragdollInstance = Instantiate(ragdollPrefab, transform.position, transform.rotation);
+            ragdollInstance.SetActive(false);
 
-            // iterate over all children of transform and copy positions and rotations to the ragdoll
+            Transform ragdollTransform = ragdollInstance.transform;
+            Transform currentTransform = transform;
 
+            ragdollTransform.rotation = currentTransform.rotation;
+            ragdollTransform.position = currentTransform.position;
+
+            while (currentTransform != null && ragdollTransform != null)
+            {
+                if (currentTransform.name == ragdollTransform.name)
+                {
+                    ragdollTransform.position = currentTransform.position;
+                    ragdollTransform.rotation = currentTransform.rotation;
+                }
+
+                if (currentTransform.childCount > 0)
+                {
+                    currentTransform = currentTransform.GetChild(0);
+                    ragdollTransform = ragdollTransform.GetChild(0);
+                }
+                else
+                {
+                    while (currentTransform != null)
+                    {
+                        if (currentTransform.parent == null || ragdollTransform.parent == null)
+                        {
+                            currentTransform = null;
+                            ragdollTransform = null;
+                        }
+                        else if (currentTransform.GetSiblingIndex() + 1 == currentTransform.parent.childCount ||
+                                 currentTransform.GetSiblingIndex() + 1 == ragdollTransform.parent.childCount)
+                        {
+                            currentTransform = currentTransform.parent;
+                            ragdollTransform = ragdollTransform.parent;
+                        }
+                        else
+                        {
+                            currentTransform = currentTransform.parent.GetChild(currentTransform.GetSiblingIndex() + 1);
+                            ragdollTransform = ragdollTransform.parent.GetChild(ragdollTransform.GetSiblingIndex() + 1);
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            ragdollInstance.SetActive(true);
             Destroy(gameObject);
         }
     }
