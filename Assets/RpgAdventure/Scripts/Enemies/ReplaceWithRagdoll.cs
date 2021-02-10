64e0f841ae232fbb883fca406c23dcb205f9656f
Filip Jerga <jerga99@gmail.com>
2021-02-10 09:22:47 +0000

    Replace with ragdoll start

---
+++ Assets/RpgAdventure/Scripts/Enemies/ReplaceWithRagdoll.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RpgAdventure
+{
+    public class ReplaceWithRagdoll : MonoBehaviour
+    {
+        public void Replace()
+        {
+            Debug.Log("Replacing with the ragdoll!");
+        }
+    }
+
+}
