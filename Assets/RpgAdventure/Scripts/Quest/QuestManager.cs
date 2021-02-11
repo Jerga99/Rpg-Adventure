743b74612aa7780c8861d0af0e9e3ef1b9c83180
Filip Jerga <jerga99@gmail.com>
2021-02-11 08:56:35 +0000

    Quest and quest manager

---
+++ Assets/RpgAdventure/Scripts/Quest/QuestManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RpgAdventure
+{
+    public class QuestManager : MonoBehaviour
+    {
+        public List<Quest> quests;
+    }
+}
