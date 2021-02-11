8c8e1d1ea0fe228ad228c04316989f5806b0411a
Filip Jerga <jerga99@gmail.com>
2021-02-11 09:26:26 +0000

    Opening JSON file

--- Assets/RpgAdventure/Scripts/Quest/QuestManager.cs
+++ Assets/RpgAdventure/Scripts/Quest/QuestManager.cs
@@ -1,11 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 namespace RpgAdventure
 {
     public class QuestManager : MonoBehaviour
     {
-        public List<Quest> quests;
+        public Quest[] quests;
+
+        private void Awake()
+        {
+            LoadQuestsFromDB();
+        }
+
+        private void LoadQuestsFromDB()
+        {
+            using (StreamReader reader = new StreamReader("Assets/RpgAdventure/DB/QuestDB.json"))
+            {
+                string json = reader.ReadToEnd();
+                quests = JsonUtility.FromJson<Quest[]>(json);
+                Debug.Log(quests);
+            }
+        }
     }
 }
