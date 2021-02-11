91beccf17d310b5a30bd0b535063b6fc4544daeb
Filip Jerga <jerga99@gmail.com>
2021-02-11 09:43:13 +0000

    Json wrapper

--- Assets/RpgAdventure/Scripts/Quest/QuestManager.cs
+++ Assets/RpgAdventure/Scripts/Quest/QuestManager.cs
@@ -5,6 +5,23 @@ using System.IO;
 
 namespace RpgAdventure
 {
+
+    public class JsonHelper
+    {
+        private class Wrapper<T>
+        {
+            public T[] array;
+        }
+
+        public static T[] GetJsonArray<T>(string json)
+        {
+            string newJson = "{ \"array\": " + json + "}";
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+            return wrapper.array;
+        }
+    }
+
+
     public class QuestManager : MonoBehaviour
     {
         public Quest[] quests;
@@ -19,8 +36,9 @@ namespace RpgAdventure
             using (StreamReader reader = new StreamReader("Assets/RpgAdventure/DB/QuestDB.json"))
             {
                 string json = reader.ReadToEnd();
-                quests = JsonUtility.FromJson<Quest[]>(json);
-                Debug.Log(quests);
+                var loadedQuests = JsonHelper.GetJsonArray<Quest>(json);
+                quests = new Quest[loadedQuests.Length];
+                quests = loadedQuests;
             }
         }
     }
