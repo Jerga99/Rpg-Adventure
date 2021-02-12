4d37cce2b435bc35cc8cac7ba4918b11e3ed78fc
Filip Jerga <jerga99@gmail.com>
2021-02-12 10:11:21 +0000

    Mouse over collider

--- Assets/RpgAdventure/Scripts/Quest/QuestGiver.cs
+++ Assets/RpgAdventure/Scripts/Quest/QuestGiver.cs
@@ -6,6 +6,17 @@ namespace RpgAdventure
     public class QuestGiver : MonoBehaviour
     {
         public Quest quest;
+
+
+        private void OnMouseEnter()
+        {
+            Debug.Log("OnMouseEnter");
+        }
+
+        private void OnMouseExit()
+        {
+            Debug.Log("OnMouseExit");
+        }
     }
 }
 
