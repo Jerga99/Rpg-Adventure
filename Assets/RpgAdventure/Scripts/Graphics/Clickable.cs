18345b0dba203de70cbd9c6577884224ecac819a
Filip Jerga <jerga99@gmail.com>
2021-02-12 10:22:38 +0000

    Clickable and cursor image

---
+++ Assets/RpgAdventure/Scripts/Graphics/Clickable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RpgAdventure
+{
+    public class Clickable : MonoBehaviour
+    {
+        public Texture2D questionCursor;
+
+        private void OnMouseEnter()
+        {
+            Debug.Log("OnMouseEnter");
+        }
+
+        private void OnMouseExit()
+        {
+            Debug.Log("OnMouseExit");
+        }
+    }
+}
