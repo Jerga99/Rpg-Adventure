e0cf437e918de61f47a8ee809ddea6cfafa5cf52
Filip Jerga <jerga99@gmail.com>
2021-02-12 10:03:58 +0000

    Quest Log

---
+++ Assets/RpgAdventure/Scripts/Quest/QuestLog.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RpgAdventure
+{
+    public enum QuestStatus
+    {
+        ACTIVE,
+        FAILED,
+        COMPLETED
+    }
+
+    public class AcceptedQuest : Quest
+    {
+        public QuestStatus questStatus;
+    }
+
+    public class QuestLog : MonoBehaviour
+    {
+        public List<AcceptedQuest> quests;
+    }
+}
