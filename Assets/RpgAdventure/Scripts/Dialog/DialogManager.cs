f1c65ee48f156bce2c4ba536c58bc4d358d51736
Filip Jerga <jerga99@gmail.com>
2021-02-15 10:36:47 +0000

    Stop dialog

--- Assets/RpgAdventure/Scripts/Dialog/DialogManager.cs
+++ Assets/RpgAdventure/Scripts/Dialog/DialogManager.cs
@@ -6,14 +6,25 @@ namespace RpgAdventure
 {
     public class DialogManager : MonoBehaviour
     {
+        public float maxDialogDistance;
         public GameObject dialogUI;
         public Text dialogHeaderText;
 
         private PlayerInput m_Player;
-        private GameObject m_Npc;
+        private QuestGiver m_Npc;
+
         private Dialog m_ActiveDialog;
 
         public bool HasActiveDialog { get { return m_ActiveDialog != null; } }
+        public float DialogDistance
+        {
+            get
+            {
+                return Vector3.Distance(
+                    m_Player.transform.position,
+                    m_Npc.transform.position);
+            }
+        }
 
         private void Start()
         {
@@ -23,30 +34,37 @@ namespace RpgAdventure
         private void Update()
         {
             if (!HasActiveDialog &&
-                m_Player != null &&
                 m_Player.OptionClickTarget != null)
             {
                 if (m_Player.OptionClickTarget.CompareTag("QuestGiver"))
                 {
-                    m_Npc = m_Player.OptionClickTarget.gameObject;
+                    m_Npc = m_Player.OptionClickTarget.GetComponent<QuestGiver>();
 
-                    var distanceToTarget = Vector3.Distance(
-                    m_Player.transform.position,
-                    m_Npc.transform.position);
-
-                    if (distanceToTarget < 2.0f)
+                    if (DialogDistance < maxDialogDistance)
                     {
                         StartDialog();
                     }
                 }
             }
+
+            if (HasActiveDialog && DialogDistance > maxDialogDistance + 1.0f)
+            {
+                StopDialog();
+            }
         }
 
         private void StartDialog()
         {
-            m_ActiveDialog = m_Npc.GetComponent<QuestGiver>().dialog;
+            m_ActiveDialog = m_Npc.dialog;
             dialogUI.SetActive(true);
             dialogHeaderText.text = m_Npc.name;
+        }
+
+        private void StopDialog()
+        {
+            m_Npc = null;
+            m_ActiveDialog = null;
+            dialogUI.SetActive(false);
         }
     }
 }
