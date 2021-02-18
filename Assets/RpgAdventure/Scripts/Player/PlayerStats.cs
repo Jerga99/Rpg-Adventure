d72c63213b52163504aa544724e7357bb02d999b
Filip Jerga <jerga99@gmail.com>
2021-02-18 09:07:31 +0000

    Player Stats

---
+++ Assets/RpgAdventure/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace RpgAdventure
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        public int maxLevel;
+        public int currentLevel;
+        public int currentExp;
+        public int[] availableLevels;
+
+        private void Awake()
+        {
+            availableLevels = new int[maxLevel];
+            ComputeLevels(maxLevel);
+        }
+
+        private void ComputeLevels(int levelCount)
+        {
+            for (int i = 0; i < levelCount; i++)
+            {
+                var level = i + 1;
+                var levelPow = Mathf.Pow(level, 2);
+                var expToLevel = Convert.ToInt32(levelPow * levelCount);
+                availableLevels[i] = expToLevel;
+            }
+        }
+    }
+}
