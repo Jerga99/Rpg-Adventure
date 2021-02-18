7a88a74d152972c78b549908ca0f0dfabb0e7466
Filip Jerga <jerga99@gmail.com>
2021-02-18 10:17:47 +0000

    Item spawner

---
+++ Assets/RpgAdventure/Scripts/Core/ItemSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RpgAdventure
+{
+    public class ItemSpawner : MonoBehaviour
+    {
+        public GameObject itemPrefab;
+
+        void Start()
+        {
+            Instantiate(itemPrefab, transform);
+            Destroy(transform.GetChild(0).gameObject);
+        }
+    }
+
+}
