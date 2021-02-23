8dae626ad57e6ec34b01d16aed93ea3612f35313
Filip Jerga <jerga99@gmail.com>
2021-02-23 09:21:43 +0000

    Random audio player

---
+++ Assets/RpgAdventure/Scripts/Audio/RandomAudioPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RpgAdventure
+{
+    public class RandomAudioPlayer : MonoBehaviour
+    {
+        [System.Serializable]
+        public class SoundBank
+        {
+            public string name;
+            public AudioClip[] clips;
+        }
+
+        public SoundBank soundBank = new SoundBank();
+        private AudioSource m_AudioSource;
+
+        private void Awake()
+        {
+            m_AudioSource = GetComponent<AudioSource>();
+        }
+    }
+}
